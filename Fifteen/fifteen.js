dabc940ff81cab66372de64e542b05bd556c8711
nik5033 <gudkov.nikita@bk.ru>
2020-09-20 01:28:27 +0300

    15

--- Fifteen/fifteen.js
+++ Fifteen/fifteen.js
@@ -1,4 +1,4 @@
 function check_email(email){
-    emails = new RegExp("[\\w0-9_-]+@[\\w0-9_-]+\\.[\\w]{2,4}",'ig');
+    let emails = new RegExp("[\\w0-9_-]+@[\\w0-9_-]+\\.[\\w]{2,4}",'ig');
     return emails.test(email);
 }
