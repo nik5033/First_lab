5ccc586d9bc5a1a29d02085d32a291bb41932993
nik5033 <gudkov.nikita@bk.ru>
2020-09-19 18:34:43 +0300

    5

---
+++ Five/five.js
@@ -0,0 +1,9 @@
+function Find (str) {
+    let words = str.split(" ");
+    let max_w = words[0];
+    for(i = 1; i < words.length; i++){
+        if(max_w.length < words[i].length)
+            max_w = words[i];
+    }
+    return max_w;
+}
