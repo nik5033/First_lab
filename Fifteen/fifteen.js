5f52c8ecc6a2332a88305ae8c10f7803d5dc9c2d
nik5033 <gudkov.nikita@bk.ru>
2020-09-25 00:09:00 +0300

    Export

--- Fifteen/fifteen.js
+++ Fifteen/fifteen.js
@@ -1,4 +1,6 @@
 function check_email(email){
-    let emails = new RegExp("[\\w0-9_-]+@[\\w0-9_-]+\\.[\\w]{2,4}",'ig');
+    let emails = new RegExp("[\\w0-9.!#$%&'*+-\\/=?^_`{|}~]+@[\\w0-9.!#$%&'*+-\\/=?^_`{|}~]+\\.[\\w0-9-]{2,4}",'i');
     return emails.test(email);
 }
+
+module.exports.check_email = check_email;
